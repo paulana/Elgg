aed30b8a8a96f5d5cb00c69424e517e90f9c7e3f
Paulana Thompson <paulana.hall@gmail.com>
2018-11-27 17:19:44 -0500

    Update settings.php

--- elgg-config/settings.php
+++ elgg-config/settings.php
@@ -36,7 +36,7 @@ if (!isset($CONFIG)) {
  *
  * @global string $CONFIG->dataroot
  */
-$CONFIG->dataroot = '\\home\\site\\data\\';
+$CONFIG->dataroot = 'D:\\home\\site\\data\\';
 
 /**
  * The installation root URL of the site. E.g. 
